c9cd5b44d44c3388e01bdf61a43e5b41089dbabe
Dan Patterson <starved14@hotmail.com>
2022-09-08 23:09:17 -0600

    fixed uri

--- server.js
+++ server.js
@@ -5,26 +5,34 @@ const morgan = require("morgan")
 const mongoose = require("mongoose")
 const cors = require("cors")
 const expressJwt = require("express-jwt")
+const path = require("path")
+
+const port = process.env.PORT || 7000;
+
+const secret = process.env.SECRET || "bottle filter ghost tray"
 
 app.use(express.json())
 app.use(cors())
 app.use(morgan("dev"))
 
-mongoose.connect("mongodb://localhost:27017/mortcalcdb",
-    {
-        useNewUrlParser: true,
-        useUnifiedTopology: true,
-        useCreateIndex: true,
-        useFindAndModify: false,
-    },
-    () => console.log("Connected to the DB.")    
-)
+// mongoose.connect("mongodb://localhost:27017/mortcalcdb",
+//     {
+//         useNewUrlParser: true,
+//         useUnifiedTopology: true,
+//         useCreateIndex: true,
+//         useFindAndModify: false,
+//     },
+//     () => console.log("Connected to the DB.")    
+// )
+mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true });
+
 app.use("/auth", require("./routes/auth.js"))
-app.use("/api", expressJwt({ secret: process.env.SECRET, algorithms: ["HS256"]}))
+app.use("/api", expressJwt({ secret: secret, algorithms: ["HS256"]}))
 app.use("/api/leads", require("./routes/leadRouter.js"))
 app.use("/api/realtors", require("./routes/realtorRouter.js"))
 app.use("/api/status", require("./routes/statusRouter.js"))
 app.use("/api/campaigns", require("./routes/campaignRouter.js"))
+app.use(express.static(path.join(__dirname, "client", "build")))
 
 app.use((err, req, res, next) => {
     console.log(err)
@@ -33,7 +41,10 @@ app.use((err, req, res, next) => {
     }
     return res.send({errMsg: err.message})
 })
+app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "client", "build", "index.html"));
+});
 
-app.listen(7000, () => {
+app.listen(port, () => {
     console.log("The server is running on Port 7000.")
 })
