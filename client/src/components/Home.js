d2effa4f952cf7b9bfbd194046d83be0927e34bb
Dan Patterson <starved14@hotmail.com>
2022-11-05 02:00:09 -0600

    fixing home page

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -5,13 +5,13 @@ function Home() {
     const context = useContext(MortgageContext)
     return (
         <div>
-            <form style={{display: "flex", justifyContent: "center"}}onSubmit={(event) => {
+            {/* <form style={{display: "flex", justifyContent: "center"}}onSubmit={(event) => {
                 event.preventDefault()
                 context.handleSubmit()
             }}>
                 <input placeholder="Client Search" type="text"name="clientsearch" value={context.clientSearch} onChange={context.handleChange}/>
                 <button type="submit">Search</button>
-            </form>
+            </form> */}
         </div>
     )
 }
